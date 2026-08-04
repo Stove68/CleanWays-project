a08de2da578856d9db8043e7ee6d895e1dc705a0
Stove68 <joerg-stoverock@idexx.com>
2026-08-04 19:11:28 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ alert("CleanWays Script läuft");
 
 document.getElementById("map").style.backgroundColor = "red";
 
-const map = L.map('map').setView([51.1657, 10.4515], 6);
+const map = L.map('map').setView([51.0, 10.0], 7);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; OpenStreetMap'
