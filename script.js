5f9746edb6dfa7c0b8f7e55acea3e8dbacd1ca20
Stove68 <joerg-stoverock@idexx.com>
2026-08-04 18:09:58 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-console.log("CleanWays gestartet");
+var map = L.map('map').setView([51.1657, 10.4515], 6);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; OpenStreetMap'
+}).addTo(map);
