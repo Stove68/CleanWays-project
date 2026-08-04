d79ede1e6a0a06a2ba91d1818de7ad31180d176b
Stove68 <joerg-stoverock@idexx.com>
2026-08-04 18:39:21 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var map = L.map('map').setView([51.1657, 10.4515], 6);
+const map = L.map('map').setView([51.1657, 10.4515], 6);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; OpenStreetMap'
