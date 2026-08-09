94a50a7c91019c60bffd713b0121633c51bc2701
Stove68 <joerg-stoverock@idexx.com>
2026-08-09 12:08:02 +0200

    Fix alert statement and adjust map initialization

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
 alert("TEST123");
-
-const map = L.map("map1.1657, 10.4515], 6);
-
-L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    attribution: "&copy; OpenStreetMap contributors"
-}).addTo(map);
-
-L.marker([51.1657, 10.4515]).addTo(map);
